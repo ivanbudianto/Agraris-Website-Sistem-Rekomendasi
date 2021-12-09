b6c8333a2e40ef4175c42d257e60a5b9dc5bea88
Mochamad Nahrul <mochnahrul.wd@gmail.com>
2021-12-09 23:38:34 +0700

    deploy crop recommendation model

--- app.py
+++ app.py
@@ -1,8 +1,55 @@
 # Import library
-from flask import Flask, render_template
+import numpy as np
+import requests
+import tensorflow as tf
+from flask import Flask, render_template, request, json
+from keras.models import load_model
 
 # Create flask app
 app = Flask(__name__)
+
+# Load crop recommendation model
+crop_recommendation_model = load_model('models/model_dir/best_model_crop_recommendation.h5')
+
+# Cast numeric label back to wordish prediction
+def recommend_label(prediction):
+  result = np.argmax(prediction)
+
+  with open('models/label_dir/recommend_dic.json', 'r') as file:
+    labels = json.load(file)
+
+  for key, value in labels.items():
+    if str(result) == value:
+      return key
+
+# Normalize input range
+def normalization(array_parameter):
+  # Value is retrieved from the df.describe()
+  min_val = [0, 5, 5, 8.825675, 14.258040, 3.504752, 20.211267]
+  max_val = [140, 145, 205, 43.675493, 99.981876, 9.935091, 298.560117]
+
+  for i in range(len(array_parameter[0])):
+    array_parameter[0][i] = (array_parameter[0][i] - min_val[i]) / (max_val[i] - min_val[i])
+
+  np_scaled = np.array(array_parameter)
+  return np_scaled
+
+# Fetch weather API
+def weather_fetch(city_name):
+  base_url = 'https://api.openweathermap.org/data/2.5/weather?q='
+  api_key = '1661559d236944b41319e5fe4c287b2e'
+  city_url = base_url + city_name + ',id' + '&appid=' + api_key
+
+  response = requests.get(city_url)
+  response_json = response.json()
+
+  if response_json['cod'] != '404':
+    main = response_json['main']
+    temperature = round((main['temp'] - 273.15), 2)
+    humidity = main['humidity']
+    return temperature, humidity
+  else:
+    return None
 
 # Render home page
 @app.route('/')
@@ -18,6 +65,26 @@ def crop_recommendation():
 @app.route('/crop-disease')
 def crop_disease():
   return render_template('disease.html')
+
+# Render predict crop recommendation page
+@app.route('/predict-crop-recommendation', methods=['POST'])
+def recommend_predict():
+  N = int(request.form['nitrogen'])
+  P = int(request.form['phosphorous'])
+  K = int(request.form['potassium'])
+  city = request.form.get('city')
+  ph = float(request.form['ph'])
+  rainfall = float(request.form['rainfall'])
+
+  if weather_fetch(city) != None:
+    temperature, humidity = weather_fetch(city)
+    user_input = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
+    scaled = normalization(user_input)
+    prediction = crop_recommendation_model.predict(scaled)
+    result = recommend_label(prediction)
+    return render_template('recommend-predict.html', prediction=result)
+  else:
+    return render_template('recommend.html')
 
 # Run flask app
 if __name__ == '__main__':
